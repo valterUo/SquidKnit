5c2a2da9f7590e2939ce0e6220539ddb64a8bef6
Uotila Valter J E <vauotila@lx2-500-123.ad.helsinki.fi>
2023-10-20 09:33:01 +0300

    results

--- experiments.py
+++ experiments.py
@@ -15,7 +15,7 @@ gates = ["cnot", "dcnot", "cz", "swap"]
 input = {"first_qubit": [], "second_qubit": ["H"]}
 results = {}
 
-link_fidelity_list = np.arange(0.5, 1.0, step=0.05)
+link_fidelity_list = np.arange(0.4, 1.05, step=0.05)
 
 for fidelity in link_fidelity_list:
     fidelity_results = {}
@@ -23,4 +23,14 @@ for fidelity in link_fidelity_list:
         fidelity_results[gate] = run_simulation(input, gate, link_fidelity = fidelity, num_times = 100)
     results[fidelity] = fidelity_results
 
-open("results.json", "w").write(json.dumps(results, indent = 4, cls=NumpyArrayEncoder))
+open("link_fidelity_results.json", "w").write(json.dumps(results, indent = 4, cls = NumpyArrayEncoder))
+
+qdevice_noise_list = np.arange(0.0, 0.55, step=0.05)
+results = {}
+for noise in qdevice_noise_list:
+    noise_results = {}
+    for gate in gates:
+        noise_results[gate] = run_simulation(input, gate, qdevice_noise = noise, num_times = 100)
+    results[noise] = noise_results
+    
+open("qdevice_noise_results.json", "w").write(json.dumps(results, indent = 4, cls = NumpyArrayEncoder))
--- run_simulation.py
+++ run_simulation.py
@@ -6,7 +6,7 @@ from config import create_two_node_network
 from squidasm.run.stack.run import run
 from utils import calculate_reference_state, get_info
 
-def run_simulation(input, gate, num_times = 1, device = "generic", link_fidelity: float = 0, qdevice_noise: float = 0):
+def run_simulation(input, gate, num_times = 1, device = "generic", link_fidelity: float = 1.0, qdevice_noise: float = 0.0):
 
     node_names = ["Gate", "Input"]
     cfg = create_two_node_network(node_names, device, link_fidelity, qdevice_noise)
